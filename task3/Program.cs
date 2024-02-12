1b435f7ab7f463125a9fe51bf5e611cb36996c11
tynchtyk1985 <turganbekov1985@mail.ru>
2024-02-12 21:06:39 +0600

    add lesson12.02:task3

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] array = { 2, 5, 7, 4, 7, 8, 35, 1, 3, 8 };
+int i = 0;
+
+while(i < n)
+{
+    if(array[i] % 2 == 0 )
+    {
+        Console.Write($"{array[i]} ");
+    }
+     i = i + 1;
+}
